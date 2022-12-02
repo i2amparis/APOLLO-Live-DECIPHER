f25693a4ba20685e526e253edf8bca5f2e4bf074
Anastasios Soursos <a.soursos@gmail.com>
2022-12-02 09:52:05 +0200

    Add/remove role from a user.

--- src/Topsis.Domain/Contracts/RoleNames.cs
+++ src/Topsis.Domain/Contracts/RoleNames.cs
@@ -1,10 +1,21 @@
-﻿namespace Topsis.Domain.Contracts
+﻿using System.Collections.Generic;
+
+namespace Topsis.Domain.Contracts
 {
     public static class RoleNames
     {
         public const string Admin = "admin";
         public const string Moderator = "moderator";
         public const string Stakeholder = "stakeholder";
-    }
 
+        public static IDictionary<string, string> All()
+        {
+            return new Dictionary<string, string>
+            {
+                { Admin, "Can manage users." },
+                { Moderator, "Can moderate workspaces." },
+                { Stakeholder, "Can answer/vote a questionnaire." }
+            };
+        }
+    }
 }
---
+++ src/Topsis.Web/Areas/Admin/Pages/User.cshtml.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Topsis.Application.Contracts.Identity;
+using Topsis.Application.Features;
+using Topsis.Domain.Contracts;
+
+namespace Topsis.Web.Areas.Admin.Pages
+{
+    public class UserModel : PageModel
+    {
+        public string Email { get; private set; }
+        public IList<string> Roles { get; private set; }
+        public string ErrorMessage { get; private set; }
+
+        public async Task OnGet([FromServices] UserManager<ApplicationUser> users,
+            [FromQuery] string email,
+            [FromQuery] string error = null)
+        {
+            var user = await users.FindByEmailAsync(email);
+            Email = user.Email;
+            Roles = await users.GetRolesAsync(user);
+            ErrorMessage = error;
+        }
+
+        public async Task<IActionResult> OnPostToggleRole([FromServices] UserManager<ApplicationUser> users,
+            string role,
+            string email, 
+            bool add)
+        {
+            var user = await users.FindByEmailAsync(email);
+            if (add)
+            {
+                await users.AddToRoleAsync(user, role);
+            }
+            else
+            {
+                if (role == RoleNames.Admin)
+                {
+                    // check if this is the last admin.
+                    var adminUsers = await users.GetUsersInRoleAsync(role);
+                    if (adminUsers.Count < 2)
+                    {
+                        return RedirectToPage("User",
+                            new
+                            {
+                                email,
+                                ts = DateTime.UtcNow.Ticks,
+                                error = "There is only one admin, you cannot remove admin role."
+                            });
+                    }
+                }
+
+                await users.RemoveFromRoleAsync(user, role);
+            }
+
+            return RedirectToPage("User", new { email, ts = DateTime.UtcNow.Ticks });
+        }
+    }
+}
