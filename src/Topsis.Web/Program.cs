c2fe8f0c2638504623d0c0dfefa1966426698d10
Anastasios Soursos <a.soursos@gmail.com>
2023-09-16 14:24:15 +0300

    Version:1.0.14 - Add daily log file (with serilog).

--- src/Topsis.Web/Program.cs
+++ src/Topsis.Web/Program.cs
@@ -1,19 +1,45 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace Topsis.Web
 {
     public class Program
     {
-        public static string Version = "1.0.13";
+        public static string Version = "1.0.14";
 
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //Read Configuration from appSettings
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            //Initialize Logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application Starting.");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The Application failed to start.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                // https://codewithmukesh.com/blog/serilog-in-aspnet-core-3-1/
+                .UseSerilog() //Uses Serilog instead of default .NET Logger
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
