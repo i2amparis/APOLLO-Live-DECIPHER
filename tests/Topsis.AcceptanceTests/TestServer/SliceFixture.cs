2e86a503a8773644f84b705abd796fc6106ef5b4
Anastasios Soursos <a.soursos@gmail.com>
2023-09-22 09:45:15 +0300

    Respawn only on public schema.

--- tests/Topsis.AcceptanceTests/TestServer/SliceFixture.cs
+++ tests/Topsis.AcceptanceTests/TestServer/SliceFixture.cs
@@ -45,7 +45,11 @@ namespace Topsis.AcceptanceTests.TestServer
 
             _checkpoint = new Checkpoint()
             {
-                TablesToIgnore = new[] { "AspNetRoles", "WsCountries", "WsJobCategories", "pg_largeobject" },
+                SchemasToInclude = new[]
+                {
+                    "public"
+                },
+                TablesToIgnore = new[] { "AspNetRoles", "WsCountries", "WsJobCategories" },
                 DbAdapter = DbAdapter.Postgres
             };
         }
